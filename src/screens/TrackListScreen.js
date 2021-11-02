aabb09cb43503e64020374c50d872bcf168573af
Nasimul Haque <alnasimul@gmail.com>
2021-11-03 00:40:14 +0600

    A lot of boilerplate

---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const TrackListScreen = () => {
+    return (
+        <View>
+            <Text style={{fontSize: 48, textAlign: 'center'}}>Track List Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({})
+
+export default TrackListScreen;
